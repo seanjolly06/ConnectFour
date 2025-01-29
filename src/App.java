e63786cb22f5810fac1932b88853e899c49b4ae5
Dan Ghofrani <professordannscc@gmail.ca>
2025-01-29 14:07:40 -0400

    write function to initialize empty board and to print it

--- src/App.java
+++ src/App.java
@@ -1,5 +1,35 @@
 public class App {
+
+    public static void printBoard(char[][] board) {
+        System.out.println("=============================");
+        for (int i = 0; i < 6; i++) {
+            // print the current row
+            char[] currentRow = board[i];
+            System.out.print("|");
+            for (int j = 0; j < 7; j++) {
+                char currentCharacter = currentRow[j];
+                System.out.print(" " + currentCharacter + " |");
+            }
+            System.out.println();
+        }
+        System.out.println("=============================");
+        System.out.println("  1   2   3   4   5   6   7");
+    }
+
+    public static char[][] getInitialBoard() {
+        char[][] board = {
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+        };
+        return board;
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        char[][] board = getInitialBoard();
+        printBoard(board);
     }
 }
