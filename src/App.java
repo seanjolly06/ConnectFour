a279478e0d2647bcc1e832bdd174fc214b9179c1
Sean <sean_jolly06@outlook.com>
2025-02-15 10:44:23 -0400

    Moved dropStone

--- src/App.java
+++ src/App.java
@@ -2,24 +2,7 @@ import java.util.Scanner;
 
 public class App {
 
-    public static void dropStone(
-            Scanner inputReader,
-            char[][] board,
-            char tokenToDrop) {
-        // this function will be responsible for asking the player
-        // to specify which column they want to drop in
-        // we have to make sure that column is not already full
-        // we can assume that the board is not completely full
-        // the reason for that is:
-        // this function is being called either for the very first time from
-        // main function, OR
-        // this function is being called right after the other player placed a stone
-        // we always check to make sure that the game is not over AFTER each turn
-        // which means if it is not the very first time this function is being called,
-        // we would have just checked if the game is over right after the other player
-        // placed their last stone
-        // since the game is not over, there must be at least one empty space
-    }
+    // moved dropStone in to the ConnectFourGame.java section
 
     public static void printBoard(char[][] board) {
         System.out.println("=============================");
